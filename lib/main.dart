ec519c7a7ac5f7bcae604fd33b6bcd3ac6b689e2
David Puentes <pipe01112009@gmail.com>
2026-01-19 14:19:46 -0500

    Redesigning message field design to begin Voice function.

--- lib/main.dart
+++ lib/main.dart
@@ -17,7 +17,7 @@ class MyApp extends StatelessWidget {
       child: MaterialApp(
         title: 'Yes No App',
         debugShowCheckedModeBanner: false,
-        theme: AppTheme(selectedColor: 1).theme(),
+        theme: AppTheme(selectedColor: 3).theme(),
         home: ChatScreen()
       ),
     );
--- lib/presentation/screens/shared/message_field.dart
+++ lib/presentation/screens/shared/message_field.dart
@@ -10,21 +10,74 @@ class MessageField extends StatelessWidget {
     final textController = TextEditingController();
     final FocusNode focusNode = FocusNode();
 
+    return Row(
+      spacing: 8,
+      children: [
+        Expanded(
+          child: MessageFieldBox(
+            focusNode: focusNode,
+            textController: textController,
+            onValue: onValue,
+          ),
+        ),
+        _MessageButton(textController: textController, onValue: onValue),
+      ],
+    );
+  }
+}
+
+class _MessageButton extends StatelessWidget {
+  const _MessageButton({
+    required this.textController,
+    required this.onValue,
+  });
+
+  final TextEditingController textController;
+  final ValueChanged<String> onValue;
+
+  @override
+  Widget build(BuildContext context) {
+    final ColorScheme colorScheme = Theme.of(context).colorScheme;
+    return SizedBox.square(
+      dimension: 50,
+      child: IconButton.filled(
+        onPressed: () {
+          final textValue = textController.value.text;
+          textController.clear();
+          onValue(textValue);
+        },
+        // icon: Icon(Icons.keyboard_voice),
+        icon: Icon(Icons.send_rounded),
+      ),
+    );
+  }
+}
+
+class MessageFieldBox extends StatelessWidget {
+  final FocusNode focusNode;
+  final TextEditingController textController;
+  final ValueChanged<String> onValue;
+
+  const MessageFieldBox({
+    super.key,
+    required this.focusNode,
+    required this.textController,
+    required this.onValue,
+  });
+
+  @override
+  Widget build(BuildContext context) {
     final outlineInputBorder = UnderlineInputBorder(
       borderSide: const BorderSide(color: Colors.transparent),
-      borderRadius: BorderRadius.circular(40),
+      borderRadius: BorderRadius.circular(50),
     );
 
     final inputDecoration = InputDecoration(
+      contentPadding: const EdgeInsets.symmetric(horizontal: 20, vertical: 13),
       hintText: 'Message',
       enabledBorder: outlineInputBorder,
       focusedBorder: outlineInputBorder,
       filled: true,
-      suffixIcon: IconButton(icon: Icon(Icons.send_rounded), onPressed: () {
-        final textValue = textController.value.text;
-        textController.clear();
-        onValue(textValue);
-      }),
     );
 
     return TextFormField(
